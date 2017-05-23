e3aed854bd26d7b02469132556abad634bf26394
Jasman Gill <jasman.gill26@gmail.com>
2017-05-23 14:23:38 -0400

    Added Board class

---
+++ src/Board.java
@@ -0,0 +1,72 @@
+/**
+ * Created by Jasman on 2017-05-23.
+ */
+public class Board {
+    public static final int ROW = 3;
+    public static final int COL = 3;
+    Cell[][] cell;
+    int curRow;
+    int curCol;
+
+    public Board(){
+        cell = new Cell[ROW][COL];
+        for(int i=0;i<ROW;i++){
+            for(int j=0;j<COL;j++){
+                cell[i][j]=new Cell(i,j);
+            }
+        }
+    }
+
+    public void init(){
+        for(int i=0;i<ROW;i++){
+            for(int j=0;j<COL;j++){
+                cell[i][j].clear();
+            }
+        }
+    }
+
+    public boolean endDraw(){
+        for(int i=0;i<ROW;i++){
+            for(int j=0;j<COL;j++){
+                if(cell[i][j].content == Seed.EMPTY){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean endWin(Seed seed){
+        if ( ((cell[curRow][0]).content == seed)&&((cell[curRow][1]).content == seed)&&((cell[curRow][2]).content == seed) ){
+            return true;
+        }
+
+        if ( ((cell[0][curCol]).content == seed)&&((cell[1][curCol]).content == seed)&&((cell[2][curCol]).content == seed) ){
+            return true;
+        }
+
+        if ( ((cell[0][0]).content == seed)&&((cell[1][1]).content == seed)&&((cell[2][2]).content == seed) ){
+            return true;
+        }
+        if ( ((cell[0][2]).content == seed)&&((cell[1][1]).content == seed)&&((cell[2][0]).content == seed) ){
+            return true;
+        }
+        return false;
+    }
+
+    public void paintBoard(){
+        for(int i=0;i<ROW;i++){
+            for(int j=0;j<COL;j++){
+                cell[i][j].paint();
+                if(j<COL-1){
+                    System.out.println('|');
+                }
+            }
+            System.out.println();
+            if(i<ROW-1){
+                System.out.println("-----------");
+            }
+
+        }
+    }
+}
