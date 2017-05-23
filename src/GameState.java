07073c6041c1136929f1d83cd5643c4bb33aff62
Jasman Gill <jasman.gill26@gmail.com>
2017-05-23 01:47:02 -0400

    Created enums

---
+++ src/GameState.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Jasman on 2017-05-23.
+ */
+public enum GameState {
+    PLAYING,DRAW,CROSS_WIN,NOT_WIN
+}
---
+++ src/Seed.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Jasman on 2017-05-23.
+ */
+public enum Seed {
+    EMPTY,CROSS,NOT
+}
